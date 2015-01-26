2b777c70d21de2953c310510a073db2c29f4ed7f
Jay Soto <jaysoto21@gmail.com>
2015-01-26 06:46:58 +0000

    Added submit button and save comments

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,6 +2,8 @@ class Place < ActiveRecord::Base
 	max_paginates_per 5	
 
 belongs_to :user
+has_many :comments
+
 validates :name, :presence => true, length: { minimum: 3 } 
 validates :address, :presence => true
 validates :description, :presence => true
